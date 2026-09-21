f803ea03579a125293636baa62fcfb15aa411531
Emy <emyvuillemin@gmail.com>
2026-09-21 16:58:40 -0400

    Adding hello world

---
+++ Atelier_1.py
@@ -0,0 +1,10 @@
+# Demander d'entrer le nom
+# Lire le input
+name = input("Qui êtes-vous? ")
+
+# Greetings
+
+print(f"Bienvenu a Cactusity {name}")
+
+ville = input("D'ou tu viens? ")
+print(f"J'adore {ville} !")
